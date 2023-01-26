72a89995b4ca3bad2ce2dc4aabe88b8a6c96d4f9
senanurtavsan <122603342+senanurtavsan@users.noreply.github.com>
2023-01-26 16:40:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner yaricap= new Scanner(System.in);
+         Scanner abc= new Scanner(System.in);
 
         System.out.print("Yarıçap Uzunluğunu Giriniz: ");
-        int r=yaricap.nextInt();
+        int r=abc.nextInt();
 
-        double Alan= 3.14 * r * r;
-        double Cevre= (2 * 3.14 * r);
+        double 𝜋=3.14;
+        double Alan= 𝜋 * r * r;
+        double Cevre= (2 * 𝜋 * r);
+
+
+        System.out.print("Merkez Açısının ÖLçüsünü Gİriniz : ");
+        double 𝛼= abc.nextDouble();
+
+        double DdAlani=  (𝜋 * (r*r) * 𝛼) / 360;
 
 
         System.out.println("Dairenin Alanı : " + Alan);
         System.out.println("Dairenin Çevresi : " + Cevre);
+        System.out.println("Daire Diliminin Alanı : " + DdAlani);
 
     }
 }
